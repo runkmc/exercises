1287ca4e9a24f7ca6c5bae07a9ee6d54f6bd1b01
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-08 09:05:21 -0800

    Add solution to problem 1 in Swift

---
+++ 01/swift/main.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func input() -> String {
+	let inputString = NSFileHandle.fileHandleWithStandardInput().availableData
+	let strData = NSString(data: inputString, encoding: NSUTF8StringEncoding)!
+	return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
+
+print("What is your name?")
+let name = input()
+print("Hello, " + name + ", nice to meet you!")
