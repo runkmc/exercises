7fc8aa09e2006efe5a54f7fdfb6bad250af4cb45
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-19 00:15:48 -0800

    Add solution to problem 4 in Ruby

---
+++ 04/ruby/main.rb
@@ -0,0 +1,9 @@
+puts "Enter a noun:"
+noun = gets.strip
+puts "Enter a verb:"
+verb = gets.strip
+puts "Enter an adjective:"
+adj = gets.strip
+puts "Enter an adverb"
+adv = gets.strip
+puts "Do you #{verb} your #{adj} #{noun} #{adv}? That's hilarious!"
