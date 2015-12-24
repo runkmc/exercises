d0b45d6cd446be1d9b68ae80bfe94f7dc840e7a0
= <kevin.mcgladdery@gmail.com>
2015-12-23 23:11:46 -0800

    Add solution to problem 5 in Ruby

---
+++ 05/ruby/main.rb
@@ -0,0 +1,9 @@
+puts "What is the first number?"
+first = gets.to_i
+puts "What is the second number?"
+second = gets.to_i
+
+puts "#{first} + #{second} = #{first + second}"
+puts "#{first} - #{second} = #{first - second}"
+puts "#{first} * #{second} = #{first * second}"
+puts "#{first} / #{second} = #{first / second}"
