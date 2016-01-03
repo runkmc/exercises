8b810176dbce631de8602bad6fc2e2a8e3e3a209
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-03 13:42:09 -0800

    Update naming scheme

--- 04/main.clj
+++
@@ -1,9 +0,0 @@
-(println "Enter a noun:")
-(let [noun (read-line)]
-  (println "Enter a verb:")
-  (let [verb (read-line)]
-    (println "Enter an adjective:")
-    (let [adj (read-line)]
-      (println "Enter an adverb:")
-      (let [adv (read-line)]
-        (println (str "Do you " verb " your " adj " " noun " " adv "? That's hilarious!"))))))
--- 04/main.rb
+++
@@ -1,9 +0,0 @@
-puts "Enter a noun:"
-noun = gets.strip
-puts "Enter a verb:"
-verb = gets.strip
-puts "Enter an adjective:"
-adj = gets.strip
-puts "Enter an adverb"
-adv = gets.strip
-puts "Do you #{verb} your #{adj} #{noun} #{adv}? That's hilarious!"
