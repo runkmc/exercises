b6835302198cff63f48158cadb53a54d9135443a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-13 11:49:45 -0800

    Refactor Haskell solution to problem 17

--- 17/haskell_17.hs
+++ 17/haskell_17.hs
@@ -1,19 +1,16 @@
 check_bac :: Double -> Double -> String -> Double -> String
-check_bac ounces weight "M" hours
-    | bac >= 0.8 = "Your BAC is " ++ (show bac) ++ ". You aren't legal to drive."
-    | otherwise = "Your BAC is " ++ (show bac) ++ ". You're legal."
-    where bac = male_bac ounces weight hours
+check_bac ounces weight gender hours
+    | bac_number >= 0.8 = "Your BAC is " ++ (show bac_number) ++ ". You aren't legal to drive."
+    | otherwise = "Your BAC is " ++ (show bac_number) ++ ". You're legal."
+    where bac_number = bac ounces weight gender hours
 
-check_bac ounces weight "F" hours
-    | bac >= 0.8 = "Your BAC is " ++ (show bac) ++ ". You aren't legal to drive."
-    | otherwise = "Your BAC is " ++ (show bac) ++ ". You're legal."
-    where bac = female_bac ounces weight hours
+bac :: Double -> Double -> String -> Double -> Double
+bac ounces weight gender hours = ((ounces * 5.14) / (weight * g)) - (0.15 * hours)
+    where g = gender_number gender
 
-male_bac :: (Fractional f) => f -> f -> f -> f
-male_bac ounces weight hours = ((ounces * 5.14) / (weight * 0.66)) - (0.15 * hours)
-
-female_bac :: (Fractional f) => f -> f -> f -> f
-female_bac ounces weight hours = ((ounces * 5.14) / (weight * 0.73)) - (0.15 * hours)
+gender_number :: String -> Double
+gender_number "M" = 0.73
+gender_number "F" = 0.66
 
 main = do
     putStrLn "Total ounces:"
