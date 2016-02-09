14ca16eea18ef6a8f4444b1a9ed6e5a77141ce6c
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-08 18:23:01 -0800

    Fix typo

--- 22/elixir_22.exs
+++ 22/elixir_22.exs
@@ -21,7 +21,7 @@ end
 
 {first_number, _}  = Integer.parse IO.gets "Enter the first number:"
 {second_number, _} = Integer.parse IO.gets "Enter the second number:"
-{third_number, _}  = Integer.parse IO.gets "Enter the second number:"
+{third_number, _}  = Integer.parse IO.gets "Enter the third number:"
 
 max = MyMax.my_max [first_number, second_number, third_number]
 IO.puts "The largest number is #{max}"
