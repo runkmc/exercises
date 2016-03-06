46c39b57493716ea936fa2a1cd5695f98d93e344
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-03-05 16:04:11 -0800

    Update Elixir solution to problem 28

--- 28/elixir_28.exs
+++ 28/elixir_28.exs
@@ -1,7 +1,18 @@
-{first, _}  = Integer.parse IO.gets "Enter a number:"
-{second, _} = Integer.parse IO.gets "Enter a number:"
-{third, _}  = Integer.parse IO.gets "Enter a number:"
-{fourth, _} = Integer.parse IO.gets "Enter a number:"
-{fifth, _}  = Integer.parse IO.gets "Enter a number:"
+defmodule AskForNumber do
+  def ask(msg, n, numbers) when n <= 1 do
+    {num,_} = Integer.parse IO.gets msg
+    [num | numbers]
+  end
 
-IO.puts "The total is #{Enum.sum [first,second,third,fourth,fifth]}"
+  def ask(msg, n, numbers) do
+    {num,_} = Integer.parse IO.gets msg
+    ask(msg, (n - 1), [num | numbers])
+  end
+
+  def ask(msg, n) do
+    ask(msg, n, [])
+  end
+end
+
+numbers = AskForNumber.ask("Enter a number: ", 5)
+IO.puts "The total is #{Enum.sum numbers}"
