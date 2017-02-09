0cc789a25102ce07bf835870c21dcbd48c017c32
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-02-09 12:49:17 -0800

    Update Swift exercise

--- 01/swift_01.swift
+++ 01/swift_01.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 print("What is your name?")
-let name = readLine()!
-print("Hello, " + name + ", nice to meet you!")
+let name = readLine()
+name.map { namestring in
+	print("Hello, " + namestring + ", nice to meet you!")
+}
