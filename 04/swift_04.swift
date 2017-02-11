5a18c17bd2267b8180ac439ba1b1ff9948a56340
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-02-11 09:22:26 -0800

    Update for Swift 3

--- 04/swift_04.swift
+++ 04/swift_04.swift
@@ -1,18 +1,12 @@
 import Foundation
 
-func input() -> String {
-	let inputString = NSFileHandle.fileHandleWithStandardInput().availableData
-	let strData = NSString(data: inputString, encoding: NSUTF8StringEncoding)!
-	return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
-}
-
 print("Enter a noun:")
-let noun = input()
+let noun = readLine()!
 print("Enter a verb:")
-let verb = input()
+let verb = readLine()!
 print("Enter a adjective:")
-let adjective = input()
+let adjective = readLine()!
 print("Enter a adverb:")
-let adverb = input()
+let adverb = readLine()!
 
 print("Do you \(verb) your \(adjective) \(noun) \(adverb)? That's hilarious!")
